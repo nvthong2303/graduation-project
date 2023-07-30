900bb27f569f9ade5866bf5a49cce7297601ad44
nvthong2303 <nvthong2303@gmail.com>
2023-07-30 10:55:00 +0700

    fix/deploy api server

--- server/app.js
+++ server/app.js
@@ -29,6 +29,27 @@ mongoDbConnection(mongoose, config, {
     useFindAndModify: false
 }).connectToMongo();
 
+
+// Add headers before the routes are defined
+app.use((req, res, next) => {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 // enabling CORS for some specific origins only.
 const corsOptions = {
     // origin : ['http://localhost:3000', 'http://localhost:3003'],
@@ -46,6 +67,7 @@ routes(app, express);
 
 // error handling middleware
 app.use(errorHandlingMiddleware);
+
 
 // expose app
 export default app;
