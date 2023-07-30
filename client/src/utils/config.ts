89badf1b72e7161885b8f00d00c75b0a4cc6e54e
nvthong2303 <nvthong2303@gmail.com>
2023-07-30 15:22:37 +0700

    fix/deploy client

--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -1,4 +1,11 @@
 export const endpointVersion = 'api/v1';
-export const serverUrl = 'http://54.255.187.216:3001';
-export const SOCKET_SERVER_URL = 'http://54.255.178.204:3002';
-export const MEDIA_SERVER_URL = 'http://13.212.210.97:3003';
+// aws
+// export const serverUrl = 'http://13.212.101.48:3001';
+// export const SOCKET_SERVER_URL = 'http://54.255.178.204:3002';
+// export const MEDIA_SERVER_URL = 'http://13.212.210.97:3003';
+// render
+export const serverUrl = 'https://nvthong2303-api-server.onrender.com';
+export const SOCKET_SERVER_URL = 'https://nvthong2303-socket-server.onrender.com';
+export const MEDIA_SERVER_URL = 'https://nvthong2303-media-server.onrender.com';
+
+
